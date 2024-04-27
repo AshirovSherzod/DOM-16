e0376261503169a6b1ea320f6950e7e085dcae0a
AshirovSherzod <ashirovsherzod614@gmail.com>
2024-04-27 06:05:18 -0700

    task done

--- js/index.js
+++ js/index.js
@@ -1,8 +1,10 @@
 const API_URL = "https://dummyjson.com"
 const cards = document.querySelector(".cards")
 const select = document.querySelector("#select")
-console.log(select);
+const search = document.querySelector(".search")
 
+
+// apidan malumot olish 
 async function getAPI(url) {
     let data = await fetch(`${url}/products?limit=12`, {
         method: "GET"
@@ -14,32 +16,14 @@ async function getAPI(url) {
 }
 getAPI(API_URL)
 
-async function mapSelect(api,id) {
-    let url = ""
-    if (id === "all") {
-        url = `${api}/products?limit=8`
-    }
-    else {
-        url = `${api}/products/category/${id}?limit=8`
-    }
-
-    const data = await fetch(`${url}`, {
-        method: "GET"
-    })
-
-    data.json()
-        .then(res => mapCards(res))
-        .catch(err => console.log(err))
-}
-mapSelect(API_URL, "all")
-
+// olingan malumotni card korinishida yoyish
 function mapCards(data) {
     let newData = ""
     data.products.forEach(product => {
         newData += `
             <div class="cards__card">
                 <div class="cards__card__img">
-                    <img src="${product.images[0]}" alt="">
+                    <img class="card__img" data-id="${product.id}" src="${product.images[0]}" alt="">
                 </div>
                 <div class="cards__card__desc">
                     <h1>${product.title}</h1>
@@ -51,16 +35,75 @@ function mapCards(data) {
     cards.innerHTML = newData
 }
 
-function mapOption(data) {
+
+// apidan categoryni olish
+async function getCategoryAPI(url) {
+    let data = await fetch(`${url}/products/categories`, {
+        method: "GET"
+    })
+    
+    data.json()
+        .then(res => mapCategory(res))
+        .catch(err => console.log(err))
+}
+getCategoryAPI(API_URL)
+
+// olingan categoryni select tegiga option sifatida qoshish
+function mapCategory(data) {
     let newData = `<option value="all">All</option>`
     data.forEach(e => {
         newData += `
             <option value="${e}">${e}</option>
         `
-    });
+    })
+
     select.innerHTML = newData
 }
 
-select.addEventListener("click", e=> {
-    mapSelect(API_URL,e.target.value)
+
+// select change bolganida categoryni filterlash uchun 
+select.addEventListener("change", e => {
+    filterSelect(API_URL,e.target.value)
+})
+
+
+async function filterSelect(url,selectName,searchValue) {
+    let api = ""
+    if (selectName === "all") {
+        if(searchValue) {
+            api = `${url}/products/search?q=${searchValue}`
+        }
+        else {
+            api = `${url}/products?limit=12`
+        }
+    }
+    else {
+        api = `${url}/products/category/${selectName}?limit=12`
+    }
+    console.log(api);
+    let data = await fetch(`${api}`, {
+        method: "GET"
+    })
+
+    data.json()
+        .then(res => mapCards(res))
+        .catch(err => console.log(err))
+}
+filterSelect(API_URL, "all")
+
+cards.addEventListener("click", e => {
+    if(e.target.className === "card__img")  {
+        let id = e.target.dataset.id
+        window.open(`./pages/product.html?id=${id}`, "_self")
+    }
+    
+})
+
+
+search.addEventListener("keyup", e=>{
+    let value = e.target.value.trim()
+    if (value) {
+        filterSelect(API_URL, "all", value)
+        select.value = "all"
+    }
 })
